ca0ac8cdae8eb1b52eb4fd44f5aa735914192ea1
Felix Leon <hierro59@gmail.com>
2023-04-22 18:34:35 -0500

    Corregido error de admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,10 +35,10 @@ class HomeController extends Controller
         if ($role[0]->role_id === 3) { // Especialista
             $datos = [];
             $specialistEmail = Auth::user()->email;
-            $specialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty')->where('email', '=', $specialistEmail)->get();
+            $specialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty', 'user_id')->where('email', '=', $specialistEmail)->get();
             $citas = DB::Table('citas')->where('specialist_id', '=', $specialist[0]->id,)->orderBy('created_at', 'DESC')->limit(5)->get();
             for ($i = 0; $i < count($citas); $i++) {
-                $myspecialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty')->where('id', '=', $citas[$i]->specialist_id)->get();
+                $myspecialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty', 'user_id')->where('id', '=', $citas[$i]->specialist_id)->get();
                 $paciente = DB::Table('users')->select('id', 'name', 'email')->where('id', '=', $citas[$i]->user_id)->get();
                 $score = Score::where('cita_id', '=', $citas[$i]->id)->get();
                 $score_customers = ScoreCustomer::where('cita_id', '=', $citas[$i]->id)->get();
@@ -52,6 +52,7 @@ class HomeController extends Controller
                         'cita_tipo' => $citas[$i]->tipo,
                         'cita_status' => $citas[$i]->status,
                         'specialist_id' => $specialist[0]->id,
+                        'specialist_user_id' => $specialist[0]->user_id,
                         'specialist_name' => $specialist[0]->name,
                         'specialist_email' => $specialist[0]->email,
                         'specialist_degree' => $specialist[0]->degree,
@@ -107,10 +108,12 @@ class HomeController extends Controller
         } else { //SuperAdmin
             $datos = [];
             $citas = DB::Table('citas')->orderBy('created_at', 'DESC')->get();
-            $specialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty')->where('status', '=', 1)->get();
+            $specialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty', 'user_id')->where('status', '=', 1)->get();
             for ($i = 0; $i < count($citas); $i++) {
-                $myspecialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty')->where('id', '=', $citas[$i]->specialist_id)->get();
+                $myspecialist = DB::Table('specialists')->select('id', 'name', 'email', 'degree', 'specialty', 'user_id')->where('id', '=', $citas[$i]->specialist_id)->get();
                 $paciente = DB::Table('users')->select('id', 'name', 'email')->where('id', '=', $citas[$i]->user_id)->get();
+                $getAvatar = UserUploadImages::where('customer_id', '=', $myspecialist[0]->user_id)->where('type', '=', 'avatar')->get();
+                (count($getAvatar) >= 1 ? $avatar = $getAvatar[0]['image_name'] : $avatar = "generic-user.png");
                 $array =
                     [
                         'cita_id' => $citas[$i]->id,
@@ -119,11 +122,18 @@ class HomeController extends Controller
                         'cita_tipo' => $citas[$i]->tipo,
                         'cita_status' => $citas[$i]->status,
                         'specialist_id' => $myspecialist[0]->id,
+                        'specialist_user_id' => $myspecialist[0]->user_id,
                         'specialist_name' => $myspecialist[0]->name,
                         'specialist_email' => $myspecialist[0]->email,
                         'specialist_degree' => $myspecialist[0]->degree,
                         'specialist_specialty' => $myspecialist[0]->specialty,
                         'paciente_name' => $paciente[0]->name,
+                        'paciente_avatar' => $avatar,
+                        'score_customers' => (isset($score_customers[0]->score) ? $score_customers[0]->score : NULL),
+                        'score_customers_commit' => (isset($score_customers[0]->commit) ? $score_customers[0]->commit : "Sin opinión."),
+                        'score_customers_date' => (isset($score_customers[0]->created_at) ? $score_customers[0]->created_at : NULL),
+                        'score' => (isset($score[0]->score) ? $score[0]->score : NULL),
+                        'role' => 'SuperAdmin'
                     ];
                 array_push($datos, $array);
             }
